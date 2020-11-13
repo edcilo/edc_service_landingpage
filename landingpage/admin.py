9ab407d0dd424b5b0e39e5ecb669460299ae5985
edcilo <edcilop@gmail.com>
2020-11-12 21:41:18 -0600

    add landing model to admin

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Landing
+
 # Register your models here.
+class LandingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'published', 'last_modified', 'created')
+    search_fields = ['name']
+
+admin.site.register(Landing, LandingAdmin)
