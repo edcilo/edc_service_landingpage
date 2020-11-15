cacfdc4e6cf637cd33caeb20b84fc4e3f9f3781d
edcilo <edcilop@gmail.com>
2020-11-15 01:46:15 -0600

    cast env vars

--- edcilo_com/environ.py
+++ edcilo_com/environ.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 env = environ.Env(
     # set casting, default value
-    APP_DEBUG=(bool, True)
+    APP_DEBUG=(bool, True),
+    APP_PORT=(int, 80),
+    APP_DB_PORT=(int,5432)
 )
 
 # two folders back (/a/b/ - 2 = /)
